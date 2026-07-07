e12cd94e993d057eb0b8b48c2c5633cfa04bf74e
Peterson Yoshioka <pmsyoshioka@gmail.com>
2026-07-07 19:12:12 -0300

    feat: Adiciona testes unitarios

--- src/test/java/com/pet/ConversorMoedasApplicationTests.java
+++ src/test/java/com/pet/ConversorMoedasApplicationTests.java
@@ -1,13 +1,41 @@
 package com.pet;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.pet.model.ResultadoConversaoDto;
+import com.pet.service.CotacaoService;
 
 @SpringBootTest
 class ConversorMoedasApplicationTests {
 
+	@Autowired
+	private CotacaoService service;
+	
 	@Test
-	void contextLoads() {
+	void deveSerDiferenteDeNull() {
+		ResultadoConversaoDto dto =	service.converterReaisParaDolares(1D);
+		assertNotNull(dto.valorConvertido());
 	}
 
+	@Test 
+	void deveSerMaiorQue0(){
+		ResultadoConversaoDto dto =	service.converterReaisParaDolares(1D);
+		assertTrue(dto.valorConvertido() > 0);
+	}
+	
+	@Test
+	void deveGarantirQueCalculoMatematicoEstaCorreto() {
+		ResultadoConversaoDto dto =	service.converterReaisParaDolares(1D);
+		Double provaReal = dto.valorConvertido() * dto.taxaCambio();
+		assertEquals(1.0, provaReal, 0.01);
+	}
 }
